1d390bb0bb5df4fbf59dafba9035ef02fc3aef95
Bowei Liu <liubowei@gmail.com>
2020-12-04 21:17:14 -0800

    "ruby"

---
+++ 5/first.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+#ARGF.each_with_index do |line, idx|
+#    print ARGF.filename, ":", idx, ";", line
+#end
+
+puts "1101010010".to_i(2)
+
+
---
+++ 5/second.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+#ARGF.each_with_index do |line, idx|
+#    print ARGF.filename, ":", idx, ";", line
+#end
+
+puts "1001010111".to_i(2)
+
+
